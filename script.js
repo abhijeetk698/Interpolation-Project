c867e7ea14f5d1086f1a5bde42462ee34f5c724b
Abhijeet Jha <35653985+abhijeetk698@users.noreply.github.com>
2019-07-20 02:36:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-// Newton divided Difference 
-var xVal=[2,3,5,9,13];
-var yVal=[2,3,5,9,13];
-var count=xVal.length;
+var parameters = {
+  target: '#myFunction',
+  data: [{
+    fn: 'sin(x)', 
+    color: 'red'
+ }],
+  grid: true,
+  yAxis: {domain: [-1, 1]},
+  xAxis: {domain: [0, 2*Math.PI]}
+};
+
+var btn=document.querySelector("#mainbttn");
+btn.addEventListener("click",function(){
+  var xValues=document.querySelector("#xValues").value.split(" ");
+  var yValues=document.querySelector("#yValues").value.split(" ");
+  var xVal=[];
+  var yVal=[];
+  for(var i=0;i<xValues.length;i++){
+    if(xValues[i]!=""){
+      xVal.push(Number(xValues[i]));
+    }
+  }
+  for(var i=0;i<yValues.length;i++){
+    if(yValues[i]!=""){
+      yVal.push(Number(yValues[i]));
+    }
+  }
+  ///********************************************* */
+  var count=xVal.length;
 var variator=[];
 variator.push(yVal[0]);
 var arr1=xVal;
@@ -12,24 +37,38 @@ for(var j=1;j<count;j++){
 }
 variator.push(arr2[0]);
 }
-console.log(variator);
-function value(x){
+/********************************/
+function value(){
 var f="";
 f+=String(variator[0]);
-var ans=variator[0];
 var multiplier=1;
 for(var i=0;i<(count-1);i++){
-    var mul=1;
     var m="(1";
     for(var j=0;j<=i;j++){
         m+="*"+"(x-"+String(arr1[j])+")";
-        mul=mul*(x-arr1[j]);
     }
     m+=")";
     f+=("+"+m+"*"+String(variator[i+1]));
-    ans+=(mul*(variator[i+1]));
 }
-console.log(f);
-return ans;
+return f;
 }
-console.log(value(5));
+/**********************************************/
+document.querySelector("#function").value=value();
+plot();
+})
+
+function plot() {
+  var f = document.querySelector("#function").value;
+  var xMin = document.querySelector("#xMin").value;
+  var xMax = document.querySelector("#xMax").value;
+  var yMin = document.querySelector("#yMin").value;
+  var yMax = document.querySelector("#yMax").value;
+  var color = document.querySelector("#color").value;
+  
+  parameters.data[0].fn = f;
+  parameters.xAxis.domain = [xMin, xMax];
+  parameters.yAxis.domain = [yMin, yMax];
+  parameters.data[0].color = color;
+  
+  functionPlot(parameters);
+}
